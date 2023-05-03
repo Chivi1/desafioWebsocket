02c189f8cd04d34c2d3ff8f4118630fca30ad4e9
chivi <paulofr016@gmail.com>
2023-05-03 12:03:55 -0300

    product manager importado not working

---
+++ src/Managers/ProductManager.js
@@ -0,0 +1,132 @@
+import fs from 'fs';
+
+
+class ProductManager {
+  constructor(path) {
+    this.path = path;
+    this.products = [];
+    this.loadProducts();
+  }
+
+  async loadProducts() {
+    try {
+      const data = await fs.promises.readFile(this.path);
+      this.products = JSON.parse(data);
+    } catch (err) {
+      this.products = [];
+      console.error(`Error al cargar productos desde archivo ${this.path}: ${err}`);
+    }
+  }
+  
+  async saveProducts() {
+    try {
+      const data = JSON.stringify(this.products);
+      await fs.promises.writeFile(this.path, data);
+      console.log(`Productos guardados en archivo ${this.path}.`);
+    } catch (err) {
+      console.error(`Error al guardar productos en archivo ${this.path}: ${err}`);
+    }
+  }
+  
+
+  async addProduct(product) {
+    const { title, description, price, thumbnail, code, stock } = product;
+    if (!title || !description || !price || !thumbnail || !code || !stock) {
+      console.error('Todos los campos son obligatorios');
+      return;
+    }
+    if (this.getProductByCode(code)) {
+      console.error('El código del producto ya existe');
+      return;
+    }
+    const id = this.products.length + 1;
+    const newProduct = { id, ...product };
+    this.products.push(newProduct);
+    await this.saveProducts();
+    console.log(`El producto ${newProduct.title} ha sido agregado con éxito.`);
+  }
+  
+
+  async getProducts() {
+    try {
+      const file = await fs.promises.readFile(this.path);
+      return JSON.parse(file.toString());
+    } catch (error) {
+      console.error(error);
+      throw error;
+    }
+  }
+
+  async getProductById(id) {
+    try {
+      const products = await this.getProducts();
+
+      const product = products.find((p) => p.id === id);
+
+      if (!product) {
+        throw new Error(`No se encontró ningún producto con id ${id}`);
+      }
+
+      return product;
+    } catch (error) {
+      console.error(error);
+      throw error;
+    }
+  }
+
+  getProductByCode(code) {
+    const product = this.products.find(p => p.code === code);
+    return product;
+  }
+
+  async updateProduct(id, productData) {
+    try {
+      const productIndex = this.products.findIndex(p => p.id === id);
+      ;
+      if (productIndex !== -1) {
+        // Copiamos el objeto
+        const productToUpdate = Object.assign({}, this.products[productIndex]);
+  
+        // Actualizamos solo las propiedades que se reciben en el objeto productData
+        Object.assign(productToUpdate, productData);
+  
+        this.products[productIndex] = productToUpdate;
+        await this.saveProducts();
+        console.log(`Producto con ID ${id} actualizado.`);
+      } else {
+        console.log(`Producto con ID ${id} no encontrado.`);
+      }
+    } catch (error) {
+      console.log(`Error al actualizar el producto con ID ${id}: ${error}`);
+    }
+  }
+  
+
+  deleteProduct(id) {
+    const index = this.products.findIndex(p => p.id === id);
+    if (index === -1) {
+      console.error('Producto no encontrado');
+      return;
+    }
+    this.products.splice(index, 1);
+    this.saveProducts();
+    console.log(`El producto con id ${id} ha sido eliminado con éxito.`);
+  }
+}
+
+export default ProductManager;
+//Ejemplo de uso
+
+const productManager = new ProductManager('./products.json');
+
+//Crear producto
+/* const newProduct = {
+    title: 'Nuevo producto',
+    description: 'Descripción del nuevo producto',
+    price: 300,
+    thumbnail: 'ruta/imagen.jpg',
+    code: 'ADBC525',
+    stock: 100
+};
+productManager.addProduct(newProduct);   */
+
--- src/app.js
+++ src/app.js
@@ -2,58 +2,97 @@
 import express from 'express';
 import handlebars from 'express-handlebars';
 import __dirname from './utils.js';
+import ProductManager from './Managers/ProductManager.js';
 import {Server} from 'socket.io'
+import path from 'path';
+
+
 
 const app = express();
-
-
 const server = app.listen (8080, ()=>console.log("listening on 8080")); 
 const io = new Server(server);
+
 
 
 app.engine('handlebars', handlebars.engine());
 app.set('views', `${__dirname}/views`);
 app.set('view engine', 'handlebars');
 
-app.get('/', (req,res)=>{
-    res.render('home',{ productos: productos })
-})
+app.use(express.static('public'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-app.get('/realtimeproducts', (req, res) => {
+const productManager = new ProductManager(path.join(__dirname, 'products.json'));
+
+app.post('/productos', async (req, res) => {
   const { nombre, precio } = req.body;
-  const producto = { nombre, precio, id: productos.length + 1 };
-  productos.push(producto);
-
-  // Emitir evento de producto creado a todos los clientes conectados
-  io.emit('productoCreado', producto);
-
-  res.render('realTimeProducts');
-
+  try {
+    const id = await productManager.addProduct({ title: nombre, price: precio });
+    const producto = await productManager.getProductById(id);
+    io.emit('productoCreado', producto);
+    res.sendStatus(201);
+  } catch (error) {
+    console.error(error);
+    res.sendStatus(500);
+  }
 });
 
-app.use(express.static('public'));
+app.get('/', async (req, res) => {
+  try {
+    // Obtener la lista de productos con el productManager
+    const productos = await productManager.getProducts();
 
-let productos = [];
+    // Renderizar la vista home.handlebars y pasarle la lista de productos
+    res.render('home', { productos });
+  } catch (error) {
+    console.error(`Error al obtener los productos: ${error.message}`);
+    res.status(500).send('Error interno del servidor');
+  }
+});
+
+
+app.get('/realtimeproducts', async (req, res) => {
+  try {
+    const productos = await productManager.getProducts();
+    const productosManager = new ProductManager(productos);
+    res.render('realtimeProducts', { productos: productosManager.getProducts() });
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Error al obtener los productos');
+  }
+});
+
 io.on('connection', (socket) => {
   console.log('Usuario conectado');
-  
-  // Emitir los productos al cliente cuando se conecte
-  socket.emit('productos', productos);
-  
-  // Escuchar los mensajes del cliente
-  socket.on('crearProducto', (producto) => {
-    // Crear el producto y enviarlo a todos los clientes conectados
-    productos.push(producto);
-    io.emit('productoCreado', producto);
-  });
-  socket.on('eliminarProducto', (producto) => {
-    // Eliminar el producto de la lista de productos
-    const index = productos.findIndex(p => p.nombre === producto.nombre && p.precio === producto.precio);
-    if (index !== -1) {
-      productos.splice(index, 1);
-      // Emitir el evento 'productoEliminado' a todos los clientes conectados
-      io.emit('productoEliminado', producto);
-    }
-  });
-  
-});
+io.on('productos', async (productos) => {
+  try {
+    const productosList = document.getElementById('productosList');
+    productosList.innerHTML = '';
+
+    // Obtener la lista de productos del ProductManager
+    const productManager = new ProductManager('productos.json');
+    const productos = await productManager.getProducts();
+
+    productos.forEach((producto) => {
+      const li = document.createElement('li');
+      li.innerText = `${producto.nombre} - ${producto.precio}`;
+
+      const button = document.createElement('button');
+      button.innerText = 'Eliminar';
+      li.appendChild(button);
+
+      button.addEventListener('click', async () => {
+        // Eliminar el producto del ProductManager
+        await productManager.deleteProduct(producto.id);
+
+        // Emitir evento de producto eliminado a todos los clientes conectados
+        socket.emit('eliminarProducto', producto);
+      });
+
+      productosList.appendChild(li);
+    });
+  } catch (error) {
+    console.error(error);
+  }
+});});
+
